ae25656afb89ef084e5b3b8416a4b73c7cd41861
Egor <klin0312@gmail.com>
2025-02-22 17:34:42 +0300

    add: ID in func update and delete

--- pkg/api/api.go
+++ pkg/api/api.go
@@ -4,6 +4,7 @@ import (
 	"GoNews/pkg/storage"
 	"encoding/json"
 	"net/http"
+	"strconv"
 
 	"github.com/gorilla/mux"
 )
@@ -28,7 +29,7 @@ func New(db storage.Interface) *API {
 func (api *API) endpoints() {
 	api.router.HandleFunc("/posts/all", api.postsHandler).Methods(http.MethodGet, http.MethodOptions)
 	api.router.HandleFunc("/posts", api.addPostHandler).Methods(http.MethodPost, http.MethodOptions)
-	api.router.HandleFunc("/posts", api.updatePostHandler).Methods(http.MethodPut, http.MethodOptions)
+	api.router.HandleFunc("/posts/{id}", api.updatePostHandler).Methods(http.MethodPut, http.MethodOptions)
 	api.router.HandleFunc("/posts", api.deletePostHandler).Methods(http.MethodDelete, http.MethodOptions)
 }
 
@@ -70,13 +71,63 @@ func (api *API) addPostHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 // Обновление публикации.
+// VER 1
+//
+//	func (api *API) updatePostHandler(w http.ResponseWriter, r *http.Request) {
+//		var p storage.Post
+//		err := json.NewDecoder(r.Body).Decode(&p)
+//		if err != nil {
+//			http.Error(w, err.Error(), http.StatusInternalServerError)
+//			return
+//		}
+//		err = api.db.UpdatePost(p)
+//		if err != nil {
+//			http.Error(w, err.Error(), http.StatusInternalServerError)
+//			return
+//		}
+//		w.WriteHeader(http.StatusOK)
+//	}
+//
+// VER 2
+//
+//	func (api *API) updatePostHandler(w http.ResponseWriter, r *http.Request) {
+//		vars := mux.Vars(r) // Получаем параметры из URL
+//		id := vars["id"]    // Получаем ID из URL
+//		var p storage.Post
+//		err := json.NewDecoder(r.Body).Decode(&p)
+//		if err != nil {
+//			http.Error(w, err.Error(), http.StatusInternalServerError)
+//			return
+//		}
+//		p.ID = id // Присваиваем ID из URL
+//		err = api.db.UpdatePost(p)
+//		if err != nil {
+//			http.Error(w, err.Error(), http.StatusInternalServerError)
+//			return
+//		}
+//		w.WriteHeader(http.StatusOK)
+//	}
+//
+// VER 3
 func (api *API) updatePostHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r) // Получаем параметры из URL
+	idStr := vars["id"] // Получаем ID как строку
+
+	// Преобразуем строку ID в int
+	id, err := strconv.Atoi(idStr)
+	if err != nil {
+		http.Error(w, "Неверный ID", http.StatusBadRequest)
+		return
+	}
+
 	var p storage.Post
-	err := json.NewDecoder(r.Body).Decode(&p)
+	err = json.NewDecoder(r.Body).Decode(&p)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
+
+	p.ID = id // Присваиваем ID из URL
 	err = api.db.UpdatePost(p)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
@@ -86,17 +137,40 @@ func (api *API) updatePostHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 // Удаление публикации.
+// VER 1
+//
+//	func (api *API) deletePostHandler(w http.ResponseWriter, r *http.Request) {
+//		var p storage.Post
+//		err := json.NewDecoder(r.Body).Decode(&p)
+//		if err != nil {
+//			http.Error(w, err.Error(), http.StatusInternalServerError)
+//			return
+//		}
+//		err = api.db.DeletePost(p)
+//		if err != nil {
+//			http.Error(w, err.Error(), http.StatusInternalServerError)
+//			return
+//		}
+//		w.WriteHeader(http.StatusOK)
+//	}
+//
+// VAR 2
 func (api *API) deletePostHandler(w http.ResponseWriter, r *http.Request) {
-	var p storage.Post
-	err := json.NewDecoder(r.Body).Decode(&p)
+	vars := mux.Vars(r) // Получаем параметры из URL
+	idStr := vars["id"] // Получаем ID из URL
+
+	// Преобразуем строку в целое число
+	id, err := strconv.Atoi(idStr)
+	if err != nil {
+		http.Error(w, "Неверный ID", http.StatusBadRequest)
+		return
+	}
+
+	err = api.db.DeletePost(storage.Post{ID: id}) // Передаем ID для удаления
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
-	err = api.db.DeletePost(p)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
+
 	w.WriteHeader(http.StatusOK)
 }
